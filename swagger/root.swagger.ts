253b90ea3fd4046d52ec99e7cd662dd8dcc1e279
Leidy Quiñones <lalis.181014@gmail.com>
2023-04-24 19:30:12 -0500

    v 0.0.1.0

---
+++ swagger/root.swagger.ts
@@ -0,0 +1,18 @@
+export default{
+
+/**
+ * 
+ * @swagger
+ * 
+ * /:
+ *  get:
+ *      summary: Obtener un mensaje de prueba
+ *      responses: 
+ *          200:
+ *              description: Has obtenido un mensaje de prueba
+ *              type: String
+ * 
+ */
+}
+
+//Con dos ** para que sea comentarios capaces de ser interpretador por swagger
